f6a1cd1063259d46d64fec5eb9efe2095f0bf0ac
Anthony Chiu <anthonychiu05@gmail.com>
2026-09-18 17:16:43 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        tCount = {}
+        sCount = {}
+        for i in range(len(s)):
+            tCount[t[i]] = 1 + tCount.get(t[i], 0)
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+        return tCount == sCount
